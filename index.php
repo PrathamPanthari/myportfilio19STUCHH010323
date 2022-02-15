58005706a72773b08975f1d9146186ba7b1e20f5
PrathamPanthari <99737605+PrathamPanthari@users.noreply.github.com>
2022-02-15 21:48:00 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,162 @@
+<!DOCTYPE html>
+<html>
+    <head>
+
+        <title>My Portfolio</title>
+        <link rel="stylesheet" type="text/css" href="style/style7.css" />
+
+    </head>
+    <body>
+        <div class="intro">
+            <div class="top">
+                <div class="mover">
+                    <span >
+                        <a class="home" href="port.php" style="font-size:20px; color: #313131;">Home</a>
+                    </span>
+                    <span4 >
+                        <a class="about" href="port.php#div2" style="padding-left:50px; font-size:20px; color: #313131;">About</a>
+                    </span4>
+                    <span2 >
+                        <a class="works" href="port.php#div1" style="padding-left:50px; font-size:20px; color: #313131;">Works</a>
+                    </span2>
+                    <span3 >
+                        <a class="contact" href="port.php#div4" style="padding-left:50px; font-size:20px; color: #313131;">Contact</a>
+                    </span3>
+                </div>
+            </div>
+        
+            <div class="first">
+                <div class="write">
+                    <h3>Pratham Panthari</h3>
+                    
+                    <t1>UI/UX Designer</t1>
+                </div>
+                <div class="image">
+                    <img src="box3.png" style="width: 600px; padding-left:203px;"/>
+                </div>
+            </div>
+                
+        </div>
+        
+        <div class="box-container" id="div2">
+            <div class="pink-box">
+                <div class="smallcream">
+                    <h3>Hi, There</h3>
+                    <h4>I am Pratham Panthari,</h4>
+                    <h5>a UI/UX Designer</h5>
+                    <h6>I am a person who is interested in</h6>
+                    <h7>the world of graphic designing.</h7>
+                    <div class="text">
+                        <h1>I help people in building there dream</h1>
+                        <h2>website.</h2>
+                    </div>
+                    <a  class="hireme" href="contact.php">
+                        
+                        <div class="hm-bn">
+                            <div class="edit" style="padding-top: 9px;">    
+                                <span>Hire Me</span>
+                            </div>
+                        </div>
+                        
+
+                    </a>
+                  
+                </div>
+
+            </div>
+            <div class="cream-box">
+                <div class="smallpink">
+                    <img src="me.png" style="width: 150px; opacity: 0.5; padding-left: 430px; padding-top: 100px;"/>
+                    
+                </div>
+            </div>
+
+            
+
+        </div>
+        <div class="recentworks" >
+                <div class="box1" id="div1">
+                    <div class="intext">
+                        <t>Busy Building</t>
+                        <t>and Designing</t>
+                        <t>beautiful digital</t>
+                        <t>products.</t>
+                        <t1>Checkout some of my recent</t1>
+                        <t2>works.</t2>
+                    <a class="button" href="works.php">
+                        <div class="click">
+                            <div class="text">
+                                <h3>Recent Works</h3>
+
+                            </div>
+                        </div>
+
+                    </a>
+
+                    
+
+                    </div>
+                    <div class="photo" style="padding: 0px 0px; margin:0px 0px; background-color:#000;">
+                    </div>
+                </div>
+        </div>
+
+        <div class="myskill">
+            <div class="set">
+                <div class="look">
+                    <h3>My Skills</h3>
+                    <t>I come well equipped HTML,CSS,Javascript. I have an altruistic and problem-solving mindset. I like to get to the</t>
+                    <t>heart of a problem and allow my instincts to guide me in making decisions.</t>
+                </div>
+                <div class="pics">
+                    <img src="pic1.jfif" style="height:190px; width:230px; padding-left:40px; border-radius:50%; "/>
+                    <img src="pic3.jfif" style="height:190px; width:250px; padding-left:100px;border-radius:50%;"/>
+                    <img src="pic2.png" style="height:190px; width:250px; padding-left:100px;border-radius:50%;"/>
+                </div>
+
+
+            </div>
+        </div>
+        <div class="myskill2">
+            <div class="set2">
+                <div class="look2">
+                    <h3>I am familiar wih programming languages like Python,C++,Java and have worked with databases as well.</h3>
+                    <t>Networking and security are areas in which i have expertise too.</t>
+                </div>
+                <div class="pics">
+                    <img src="pic4.jfif" style="height:190px; width:230px; padding-left:40px; border-radius:50%; "/>
+                    <img src="pic5.jfif" style="height:190px; width:250px; padding-left:100px;border-radius:50%;"/>
+                    <img src="pic6.jfif" style="height:190px; width:250px; padding-left:100px;border-radius:50%;"/>
+                </div>
+            </div>
+            
+             
+        </div>
+        <div class="box2" >
+            <div class="mycontact"id="div4">
+                <div class="details">
+                    <h3>Contact Me</h3>
+                </div>
+                
+                <form method="POST">
+                    <input type="text" name="name" placeholder="Name" required>
+                    <input type="email" name="email" placeholder="Email" required>
+                    <input type="number" name="number" placeholder="Number" required>
+                    <input type="submit" name="submit" value="SUBMIT" required>
+                </form>
+
+                
+                
+
+                
+            </div>
+
+        </div>
+
+        
+
+        <div class="bottom">
+           <h3> <span>&copy 2022 Pratham Panthari</span></h3>
+            
+        </div>
+    </body>
